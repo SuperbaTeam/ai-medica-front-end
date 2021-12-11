03d482528fe0eb5d1cbcac20e0e4977fb6541d32
Jehadabuawwad <jehadabuawwad@outlook.com>
2021-12-11 23:07:31 +0200

    feat : discover page dropdown list added

--- components/DiscoverDropDown.js
+++ components/DiscoverDropDown.js
@@ -1,10 +1,64 @@
+import { Fragment } from 'react'
+import { Menu, Transition } from '@headlessui/react'
+import { ChevronDownIcon } from '@heroicons/react/solid'
 
-
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
 
 export default function DiscoverDropDown() {
-    return (
-    <>
-    
-    </>
-    );
+  return (
+    <Menu as="div" className="relative inline-block text-left -mt-96">
+      <div>
+        <Menu.Button className="inline-flex justify-center w-96 h-auto rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm text-4xl text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
+          Select Desiese
+          <ChevronDownIcon
+            className="-mr-1 ml-2 h-10 w-10"
+            aria-hidden="true"
+          />
+        </Menu.Button>
+      </div>
+
+      <Transition
+        as={Fragment}
+        enter="transition ease-out duration-100"
+        enterFrom="transform opacity-0 scale-95"
+        enterTo="transform opacity-100 scale-100"
+        leave="transition ease-in duration-75"
+        leaveFrom="transform opacity-100 scale-100"
+        leaveTo="transform opacity-0 scale-95"
+      >
+        <Menu.Items className="origin-top-right absolute right-0 mt-2 w-96 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+          <div className="py-5">
+            <Menu.Item>
+              {({ active }) => (
+                <a
+                  href="#"
+                  className={classNames(
+                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                    'block px-6 py-2 text-3xl',
+                  )}
+                >
+                  Stroke
+                </a>
+              )}
+            </Menu.Item>
+            <Menu.Item>
+              {({ active }) => (
+                <a
+                  href="#"
+                  className={classNames(
+                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                    'block px-6 py-2 text-3xl',
+                  )}
+                >
+                  Hepatitis
+                </a>
+              )}
+            </Menu.Item>
+          </div>
+        </Menu.Items>
+      </Transition>
+    </Menu>
+  )
 }
