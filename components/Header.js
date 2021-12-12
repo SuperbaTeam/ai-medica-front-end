a54093dae0fac90648f0a997157d4e0e854b04fd
shahd1995913 <shahdalkhatib95@gmail.com>
2021-12-12 09:31:48 +0400

    Finish from design and add colore in the Header

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,30 @@
-
-
-
 export default function Header() {
-    return (
-    <>
     
-    </>
+    return (
+      
+
+        <header  style={{
+            padding:'2px 1px' ,  overflow: 'hidden' , borderRadius:'4px' , lineHeight:'25px' , padding:'12px'
+            ,float:'left' , top:'0' , position:'absolute' , 
+            width:'1550px' , backgroundColor:"#46b69b"
+        }}>
+         
+           
+          <button  style={{ float:"left" ,  marginRight:"200px",width:"100px" , backgroundColor:"#edfc44"}}> Logo</button>
+            <button  style={{ float:"left" ,  marginRight:"10px",width:"80px" , backgroundColor:"#edfc44", }}><i  style={{  marginRight:"10px",}}class="fa fa-home"></i>Home</button>
+            <button  style={{ float:"left" ,  marginRight:"10px",width:"70px" , backgroundColor:"#edfc44" }} >Discover</button>
+            <button  style={{ float:"left" ,  marginRight:"10px",width:"100px" , backgroundColor:"#edfc44"}} >Help Others</button>
+            <button    style={{ float:"right" ,  marginRight:"100px",width:"70px" , backgroundColor:"#edfc44"}}  >Signup</button>
+            <button  style={{ float:"right" ,  marginRight:"50px",width:"70px" , backgroundColor:"#edfc44" }}   >Login</button>
+             <button   style={{ float:"right" , marginRight:"50px",width:"80px" , backgroundColor:"#edfc44" , 
+          
+            }}
+            > Result</button> 
+           
+           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
+                
+        </header>
+
+
     );
 }
