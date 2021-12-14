bd86d7403847600faa33fa34e74eea7c3f66bc37
shahd1995913 <shahdalkhatib95@gmail.com>
2021-12-14 18:12:08 +0400

    feat : done from about info team final design

--- components/AboutTeam.js
+++ components/AboutTeam.js
@@ -7,18 +7,112 @@ export default function AboutTeam() {
  
 <div><p class="text-center font-serif text-3xl font-thin">Our Team</p>
 
-<div class="columns-4 bg-center">
-    <><img  class="shadow-xl rounded-full ..."src="../static/img1.jpg" alt="shahd" width="200px" height="200px" />
-    <p class="text-center font-semibold ">Shahed Alkhatib</p></>
-    <><img  class="shadow-xl rounded-full ..."src="../static/img2.jpg" alt="shahd" width="200px" height="200px" />
-    <p class="text-center font-semibold ">Shahed Alkhatib</p></>
-    <><img  class="shadow-xl rounded-full ..."src="../static/img1.jpg" alt="shahd" width="200px" height="200px" />
-    <p class="text-center font-semibold ">Shahed Alkhatib</p></>
-    <><img  class="shadow-xl rounded-full ..."src="../static/img2.jpg" alt="shahd" width="200px" height="200px" />
-    <p class="text-center font-semibold ">Shahed Alkhatib</p></>
+{/* <div class="columns-4 bg-center">
+    <><img  class="shadow-xl rounded-full ..."src="../static/Fasial.jpg" alt="shahd" width="200px" height="200px" />
+    <p class="text-center font-semibold " </p></>
+    <><img  class="shadow-xl rounded-full ..."src="../static/Jehad.jpg" alt="shahd" width="200px" height="200px" />
+    <p class="text-center font-semibold ">Jehad Abu Awwad </p></>
+    <><img  class="shadow-xl rounded-full ..."src="../static/Musab.jpg" alt="shahd" width="200px" height="200px" />
+    <p class="text-center font-semibold ">Musa'b Shalaldeh </p></>
+    <><img  class="shadow-xl rounded-full ..."src="../static/shahed.jpg" alt="shahd" width="200px" height="200px" />
+    <p class="text-center font-semibold ">Shahed Alkhateeb </p></>
 
+</div> */}
+
+
+<div class="bg-white">
+  <div class="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+    <h2 class="text-center text-2xl font-extrabold tracking-tight text-gray-900">Our Team</h2>
+
+    <div class="columns-4 mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+      <div class="group relative">
+        <div class="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
+          <img src="../static/Fasial.jpg" alt="Fasial" class="w-full h-full object-center object-cover lg:w-full lg:h-full"/>
+        </div>
+        <div class="mt-4 flex justify-between">
+          <div>
+            <h3 class="text-sm text-gray-700">
+              <a href="#">
+                <span aria-hidden="true" class="absolute inset-0"></span>
+                Faisal Kushha
+              </a>
+            </h3>
+            <p class="mt-1 text-sm text-gray-500">Software Developer</p>
+          </div>
+        
+        </div>
+      </div>
+
+       <div class="group relative">
+        <div class="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
+          <img src="../static/Jehad.jpg" alt="Jehad" class="w-full h-full object-center object-cover lg:w-full lg:h-full"/>
+        </div>
+        <div class="mt-4 flex justify-between">
+          <div>
+            <h3 class="text-sm text-gray-700">
+              <a href="#">
+                <span aria-hidden="true" class="absolute inset-0"></span>
+                Jehad Abu Awwad
+              </a>
+            </h3>
+            <p class="mt-1 text-sm text-gray-500">Software Developer</p>
+          </div>
+         
+        </div>
+      </div>
+      <div class="group relative">
+        <div class="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
+          <img src="../static/Musab.jpg" alt="Musab." class="w-full h-full object-center object-cover lg:w-full lg:h-full"/>
+        </div>
+        <div class="mt-4 flex justify-between">
+          <div>
+            <h3 class="text-sm text-gray-700">
+              <a href="#">
+                <span aria-hidden="true" class="absolute inset-0"></span>
+                Musa'b Shalaldeh
+              </a>
+            </h3>
+            <p class="mt-1 text-sm text-gray-500">Software Developer</p>
+          </div>
+        
+        </div>
+      </div>
+
+     
+      <div class="group relative">
+        <div class="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
+          <img src="../static/shahdalkha.jpg" alt="shahed" class="w-full h-full object-center object-cover lg:w-full lg:h-full"/>
+        </div>
+        <div class="mt-4 flex justify-between">
+          <div>
+            <h3 class="text-sm text-gray-700">
+              <a href="#">
+                <span aria-hidden="true" class="absolute inset-0"></span>
+                Shahed Alkhatib
+              </a>
+            </h3>
+            <p class="mt-1 text-sm text-gray-500">Software Developer</p>
+          </div>
+         
+        </div>
+ 
+   
+      
+   
+   
+   
+    </div>
+ 
+ 
+ 
+ 
+ 
+  </div>
 </div>
 
+
+ 
+</div>
 </div>
 
 
@@ -27,13 +121,4 @@ export default function AboutTeam() {
 
 
 
-
-
-
-
-
-    );
-
-
-
-}
+    );}
