235b559c3c5ceb6465b7fd2eb11397cf564d9ee9
Jehadabuawwad <jehadabuawwad@outlook.com>
2021-12-15 12:54:00 +0200

    fix : login process fixing

--- components/sub_components/LoginModal.js
+++ components/sub_components/LoginModal.js
@@ -1,29 +1,29 @@
 export default function LoginModal(props) {
-    const loginUser = (event) =>{
-        event.preventDefault();
-        const username = event.target.username.value;
-        const email = event.target.email.value;
-        const pass = event.target.password.value;
-        try{
-             props.login(username,email,pass)
+  const loginUser = (event) => {
+    event.preventDefault();
+    const username = event.target.username.value;
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+    try {
+      props.login(username, email, password)
 
-        }
-        catch{
-
-        }
-        finally{
-            props.hide()
-        }
     }
-  };
+    catch {
+
+    }
+    finally {
+      props.hide()
+    }
+  }
+
 
   return (
     <>
-      <div className="fixed z-10 inset-0 overflow-y-auto">
-        <div className="flex justify-center h-screen items-center bg-gray-200 antialiased">
-          <div className="flex flex-col w-11/12 sm:w-5/6 lg:w-1/2 max-w-2xl mx-auto rounded-lg border border-gray-300 shadow-xl">
+      <div className="fixed inset-0 z-10 overflow-y-auto">
+        <div className="flex items-center justify-center h-screen antialiased bg-gray-200">
+          <div className="flex flex-col w-11/12 max-w-2xl mx-auto border border-gray-300 rounded-lg shadow-xl sm:w-5/6 lg:w-1/2">
             <div className="flex flex-row justify-between p-6 bg-white border-b border-gray-200 rounded-tl-lg rounded-tr-lg">
-              <p className="font-semibold text-gray-800 text-4xl">Login</p>
+              <p className="text-4xl font-semibold text-gray-800">Login</p>
               <svg
                 className="w-6 h-6"
                 fill="none"
@@ -45,20 +45,20 @@ export default function LoginModal(props) {
                 <p className="mb-2 font-semibold text-gray-700">Username</p>
                 <input
                   type="text"
-                  className="p-5 mb-5 bg-white border border-gray-200 rounded shadow-sm h-10"
+                  className="h-10 p-5 mb-5 bg-white border border-gray-200 rounded shadow-sm"
                   id="username"
                   name="username"
                 />
               </div>
               <div className="flex flex-col px-6 py-5 bg-gray-50">
                 <p className="mb-2 font-semibold text-gray-700">Email</p>
-                <input type="text"className="h-10 p-5 mb-5 bg-white border border-gray-200 rounded shadow-sm" id="email" name="email"/>
+                <input type="text" className="h-10 p-5 mb-5 bg-white border border-gray-200 rounded shadow-sm" id="email" name="email" />
               </div>
               <div className="flex flex-col px-6 py-5 bg-gray-50">
                 <p className="mb-2 font-semibold text-gray-700">Password</p>
                 <input
                   type="password"
-                  className="p-5 mb-5 bg-white border border-gray-200 rounded shadow-sm h-10"
+                  className="h-10 p-5 mb-5 bg-white border border-gray-200 rounded shadow-sm"
                   id="password"
                   name="password"
                 />
@@ -71,7 +71,7 @@ export default function LoginModal(props) {
                   Cancel
                 </button>
                 <button
-                  className="px-4 py-2 text-black font-semibold bg-lime-300 rounded"
+                  className="px-4 py-2 font-semibold text-black rounded bg-lime-300"
                   type="submit"
                 >
                   Login
@@ -83,4 +83,5 @@ export default function LoginModal(props) {
       </div>
     </>
   );
+
 }
