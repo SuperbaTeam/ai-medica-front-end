eacfbcf14bc04ea573232100c9a64a6f08e9366a
Jehadabuawwad <jehadabuawwad@outlook.com>
2021-12-18 16:24:19 +0200

    feat : register feature added

--- components/sub_components/SignupModal.js
+++ components/sub_components/SignupModal.js
@@ -1,25 +1,34 @@
 export default function SignUpModal(props) {
-  const signupUser = (event) => {
+  const signupUser = async (event) => {
     event.preventDefault();
 
     const newUser = {
-      name: event.target.name.value,
       username: event.target.username.value,
       email: event.target.email.value,
-      mobile: event.target.mobile.value,
       password: event.target.password.value,
     };
-    console.log(newUser);
-    props.signup(/*Pass here the info*/);
+    console.log(newUser)
+    try {
+      await props.signup(newUser.username,newUser.email,newUser.password);
+
+    }
+    catch{
+      console.log("error in signup")
+    }
+    finally {
+      props.hide()
+      await props.showlogin()
+    }
+    
   };
 
   return (
     <>
-      <div className="fixed z-10 inset-0 overflow-y-auto">
-        <div className="flex justify-center h-screen items-center bg-gray-200 antialiased">
-          <div className="flex flex-col w-11/12 sm:w-5/6 lg:w-1/2 max-w-2xl mx-auto rounded-lg border border-gray-300 shadow-xl">
+      <div className="fixed inset-0 z-10 overflow-y-auto">
+        <div className="flex items-center justify-center h-screen antialiased bg-gray-200">
+          <div className="flex flex-col w-11/12 max-w-2xl mx-auto border border-gray-300 rounded-lg shadow-xl sm:w-5/6 lg:w-1/2">
             <div className="flex flex-row justify-between p-6 bg-white border-b border-gray-200 rounded-tl-lg rounded-tr-lg">
-              <p className="font-semibold text-gray-800 text-4xl">
+              <p className="text-4xl font-semibold text-gray-800">
                 Create Account
               </p>
               <svg
@@ -40,20 +49,10 @@ export default function SignUpModal(props) {
             </div>
             <form action="/" onSubmit={signupUser}>
               <div className="flex flex-col px-6 py-5 bg-gray-50">
-                <p className="mb-2 font-semibold text-gray-700">Name</p>
-                <input
-                  type="text"
-                  className="p-5 mb-5 bg-white border border-gray-200 rounded shadow-sm h-10"
-                  id="name"
-                  name="name"
-                  placeholder="Please enter your full name"
-                />
-              </div>
-              <div className="flex flex-col px-6 py-5 bg-gray-50">
                 <p className="mb-2 font-semibold text-gray-700">Username</p>
                 <input
                   type="text"
-                  className="p-5 mb-5 bg-white border border-gray-200 rounded shadow-sm h-10"
+                  className="h-10 p-5 mb-5 bg-white border border-gray-200 rounded shadow-sm"
                   id="username"
                   name="username"
                 />
@@ -62,27 +61,17 @@ export default function SignUpModal(props) {
                 <p className="mb-2 font-semibold text-gray-700">Email</p>
                 <input
                   type="email"
-                  className="p-5 mb-5 bg-white border border-gray-200 rounded shadow-sm h-10"
+                  className="h-10 p-5 mb-5 bg-white border border-gray-200 rounded shadow-sm"
                   id="email"
                   name="email"
                 />
               </div>
-              <div className="flex flex-col px-6 py-5 bg-gray-50">
-                <p className="mb-2 font-semibold text-gray-700">
-                  Mobile Number
-                </p>
-                <input
-                  type="number"
-                  className="p-5 mb-5 bg-white border border-gray-200 rounded shadow-sm h-10"
-                  id="mobile"
-                  name="mobile"
-                />
-              </div>
+
               <div className="flex flex-col px-6 py-5 bg-gray-50">
                 <p className="mb-2 font-semibold text-gray-700">Password</p>
                 <input
                   type="password"
-                  className="p-5 mb-5 bg-white border border-gray-200 rounded shadow-sm h-10"
+                  className="h-10 p-5 mb-5 bg-white border border-gray-200 rounded shadow-sm"
                   id="password"
                   name="password"
                 />
@@ -95,7 +84,7 @@ export default function SignUpModal(props) {
                   Cancel
                 </button>
                 <button
-                  className="px-4 py-2 text-black font-semibold bg-lime-300 rounded"
+                  className="px-4 py-2 font-semibold text-black rounded bg-lime-300"
                   type="submit"
                 >
                   Sign Up
